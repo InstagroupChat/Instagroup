b222035c0d07231e103e3aa3be2001a19ccbcbd4
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2024-02-23 13:06:49 -0800

    Create APISend.js

---
+++ APISend.js
@@ -0,0 +1,3 @@
+function sendMsgUser(username, password, message, serverid) {
+  window.open("https://script.google.com/macros/s/AKfycbz8FQ5PAa-St6fWKLJpg7NLi35gKnhzf4ph6cpHSIm0cwWADQfPJYYPIG0yEvJoTYNP/exec?username="+username+"&password="+password+"&message="+message+"&serverid="+serverid, "sending...")
+}
