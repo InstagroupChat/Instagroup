7875205c33c0240d97a03950a6c43b0ea9eb5199
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2024-02-23 17:56:17 -0800

    Update APISend.js

--- APISend.js
+++ APISend.js
@@ -1,3 +1,3 @@
 function sendMsgUser(username, password, message, serverid) {
-  window.open("https://script.google.com/macros/s/AKfycbzaUVznHzmLCtB9cbX3tjZbxyAjqKOhM-Y3H6oTAsjfZ2ryTu6SYfMl3Z9sIX6HSHJL/exec?username="+username+"&password="+password+"&message="+message+"&serverid="+serverid, "sending...")
+  window.open("https://script.google.com/macros/s/AKfycbwQvCt_z6AkYF5_EQmhgMgN_rAcluJ0cyvP6B49DbajMOlDOhS0sdj4Ug1R5unSn3mQ/exec?username="+username+"&password="+password+"&message="+message+"&serverid="+serverid, "sending...")
 }
