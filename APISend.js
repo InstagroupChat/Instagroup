d347bb7caf3345f7d4fc6b1da5b3a92f8eb2a916
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2024-03-03 10:15:34 -0800

    Update APISend.js

--- APISend.js
+++ APISend.js
@@ -1,4 +1,4 @@
 function sendMsgUser(username, password, message, serverid) {
   // This sends the message to apps script aka the server and stuff to be authenticated
-  window.open("https://script.google.com/macros/s/AKfycbzfQcW-qo5J65GHTpIsEftpiuYM_P-2CiuPaK873gtB7BCS_r3v6NVbhwOvcHnybkg7/exec?username="+username+"&password="+password+"&message="+message+"&serverid="+serverid, "sending...")
+  window.open("https://script.google.com/macros/s/AKfycbysTK_NelS1Kb5N0K0J0dW9PKKneItI0WUe0yzzZExLAKjvmaj1mRWWlqaD214zRRH6/exec?username="+username+"&password="+password+"&message="+message+"&serverid="+serverid, "sending...")
 }
